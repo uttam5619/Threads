bf85ccb28a07caa84f6ecb879b6e6cff18341883
Uttam Kumar <uttamkumar050519@gmail.com>
2024-04-16 10:48:16 +0530

    user model added

---
+++ server/src/models/user.model.js
@@ -0,0 +1,82 @@
+import { Schema, model } from 'mongoose'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+
+const userSchema = new Schema({
+    name:{
+        type: String,
+        trim: true,
+        required:[true, 'name is requiered'],
+        lowercase: true,
+        minLength: [3, 'the name should contain atleast 3 characters'],
+        maxLength: [60, 'the username should not contain more than 60 characters'],
+    },
+    username:{
+        type: String,
+        trim: true,
+        required:[true, 'name is requiered'],
+        lowercase: true,
+        type:unique,
+        minLength: [3, 'the name should contain atleast 3 characters'],
+        maxLength: [60, 'the name should not contain more than 60 characters'],
+    },
+    email :{
+        type: String,
+        trim: true,
+        required:[true, 'email is requiered'],
+        unique: true,
+        index: true,
+    },
+    password:{
+        type: String,
+        required:[true, 'password is requiered'],
+        trim: true,
+        lowercase: true,
+        select:false,
+        minLength: [6, 'the password should contain at least 6 characters'],
+        maxLength: [100, 'the password should not contain more than 100 characters'],
+    },
+    avatar:{
+        secure_url:{
+            type:String, //cloudinary url
+            required:true
+        },
+        public_id:{
+            type:String, //cloudinary url
+            required:true
+        }
+    },
+    followers:{
+        type:String,
+        default:0
+    },
+    following:{
+        type:String,
+        default:0
+    },
+    description:{
+        type:String,
+        trim:true,
+        lowercase:true,
+        maxLength:[150, 'the description should not be greater than 150 characters']
+    }
+},{timestamps:true})
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next()
+    this.password = await bcrypt(this.password,10)
+    next()
+})
+
+userSchema.methods ={
+    generateToken:async function(){
+        return await jwt.sign(
+            {id:this._id, username:this.username, email:this.email},
+            process.env.ACCESS_TOEKN_SECRET,
+            {expiresIn:'5m'}
+        )
+    },
+    comparePassword: async function(password){
+        return await bcrypt.compare(password,this.password)
+    }
+}
